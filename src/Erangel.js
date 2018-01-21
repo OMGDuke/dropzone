bcd3019788599069cb79196f800a4b02d148ffbb
OMGDuke <lex1989@gmail.com>
2018-01-21 23:09:14 +0000

    Added more locations

--- src/Erangel.js
+++ src/Erangel.js
@@ -1,14 +1,4 @@
 const erangelData = {
-  locations: [
-    {
-      name: 'Power',
-      coord: 'A,L',
-    },
-    {
-      name: 'Georgopol',
-      coord: 'B,K',
-    },
-  ],
   grid: [
     'A,I', 'B,I', 'C,I', 'D,I', 'E,I', 'F,I', 'G,I', 'H,I',
     'A,J', 'B,J', 'C,J', 'D,J', 'E,J', 'F,J', 'G,J', 'H,J',
@@ -18,6 +8,108 @@ const erangelData = {
     'A,N', 'B,N', 'C,N', 'D,N', 'E,N', 'F,N', 'G,N', 'H,N',
     'A,O', 'B,O', 'C,O', 'D,O', 'E,O', 'F,O', 'G,O', 'H,O',
     'A,P', 'B,P', 'C,P', 'D,P', 'E,P', 'F,P', 'G,P', 'H,P',
+  ],
+  locations: [
+    {
+      name: 'Farm',
+      coord: 'F,M',
+    },
+    {
+      name: 'Ferry Pier',
+      coord: 'C,N',
+    },
+    {
+      name: 'Gatka',
+      coord: 'C,L',
+    },
+    {
+      name: 'Georgopol',
+      coord: 'B,K',
+    },
+    {
+      name: 'Hospital',
+      coord: 'B,L',
+    },
+    {
+      name: 'Lipovka',
+      coord: 'G,L',
+    },
+    {
+      name: 'Mansion',
+      coord: 'G,K',
+    },
+    {
+      name: 'Mylta',
+      coord: 'F,M',
+    },
+    {
+      name: 'Mylta Power Large',
+      coord: 'H,M',
+    },
+    {
+      name: 'Mylta Power Mini',
+      coord: 'G,M',
+    },
+    {
+      name: 'Novorepnoye',
+      coord: 'F,N',
+    },
+    {
+      name: 'Pochinki',
+      coord: 'D,L',
+    },
+    {
+      name: 'Primorsk',
+      coord: 'B,N',
+    },
+    {
+      name: 'Prison',
+      coord: 'G,L',
+    },
+    {
+      name: 'Quarry',
+      coord: 'B,N',
+    },
+    {
+      name: 'Rozhok',
+      coord: 'D,K',
+    },
+    {
+      name: 'Ruins',
+      coord: 'D,L',
+    },
+    {
+      name: 'Sosnovka Military Base',
+      coord: 'E,O',
+    },
+    {
+      name: 'School',
+      coord: 'E,L',
+    },
+    {
+      name: 'Severny',
+      coord: 'D,J',
+    },
+    {
+      name: 'Shelter',
+      coord: 'F,L',
+    },
+    {
+      name: 'Shooting Range',
+      coord: 'D,J',
+    },
+    {
+      name: 'Stalber',
+      coord: 'F,J',
+    },
+    {
+      name: 'Yasnaya Polyana',
+      coord: 'F,K',
+    },
+    {
+      name: 'Zharki',
+      coord: 'B,J',
+    },
   ],
 };
 

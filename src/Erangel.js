ca888a2d5f87a1687a19f44bca480b985f19b047
OMGDuke <lex1989@gmail.com>
2018-01-21 22:28:32 +0000

    Now chooses from an array of drop locations and highlights the square

---
+++ src/Erangel.js
@@ -0,0 +1,24 @@
+const erangel = {
+  locations: [
+    {
+      name: 'Power',
+      coord: 'A,L',
+    },
+    {
+      name: 'Georgopol',
+      coord: 'B,K',
+    },
+  ],
+  grid: [
+    'A,I', 'B,I', 'C,I', 'D,I', 'E,I', 'F,I', 'G,I', 'H,I',
+    'A,J', 'B,J', 'C,J', 'D,J', 'E,J', 'F,J', 'G,J', 'H,J',
+    'A,K', 'B,K', 'C,K', 'D,K', 'E,K', 'F,K', 'G,K', 'H,K',
+    'A,L', 'B,L', 'C,L', 'D,L', 'E,L', 'F,L', 'G,L', 'H,L',
+    'A,M', 'B,M', 'C,M', 'D,M', 'E,M', 'F,M', 'G,M', 'H,M',
+    'A,N', 'B,N', 'C,N', 'D,N', 'E,N', 'F,N', 'G,N', 'H,N',
+    'A,O', 'B,O', 'C,O', 'D,O', 'E,O', 'F,O', 'G,O', 'H,O',
+    'A,P', 'B,P', 'C,P', 'D,P', 'E,P', 'F,P', 'G,P', 'H,P',
+  ],
+};
+
+export default erangel;
---
+++ src/components/Map.vue
@@ -0,0 +1,60 @@
+<template>
+ <div class="map">
+   <img class="map__image" src="@/assets/erangel-map.png/" alt="">
+   <div class="grid">
+    <template v-for="coord in gridCoords">
+      <div class="grid__square" :key="coord"
+        :class="{'grid__square--selected': coord === selectedSquare.coord}"></div>
+    </template>
+   </div>
+   <h1>Dropping in {{selectedSquare.name}}</h1>
+ </div>
+</template>
+
+<script>
+import erangel from '../Erangel';
+
+export default {
+  name: 'Map',
+  data() {
+    return {
+      gridCoords: erangel.grid,
+      locations: erangel.locations,
+      selectedSquare: this.selectSquare(),
+    };
+  },
+  methods: {
+    selectSquare() {
+      const location = erangel.locations[Math.floor(Math.random() * erangel.locations.length)];
+      return location;
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .map {
+    height: 800px;
+    width: 800px;
+  }
+  .map__image {
+    height: inherit;
+    position: absolute;
+    z-index: -10;
+  }
+  .grid {
+    width: 800px;
+    display: grid;
+    grid-template-columns: repeat(8, 100px);
+    grid-template-rows: repeat(8, 100px);
+  }
+
+  .grid__square {
+    border: 1px greenyellow solid;
+  }
+
+  .grid__square--selected {
+    background: rgba(0, 256, 0, 0.5);
+  }
+</style>

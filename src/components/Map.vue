b68e32d3816114220df0316daeaf3572c3809555
Alex Robertson <lex1989@gmail.com>
2018-01-22 18:32:39 +0000

    Added refresh button

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,14 +1,32 @@
 <template>
- <div class="map">
-   <img class="map__image" :src="mapImage" alt="">
-   <div class="grid">
-    <template v-for="coord in mapData.grid">
-      <div class="grid__square" :key="coord"
-        :class="{'grid__square--selected': coord === selectedSquare.coord}"></div>
-    </template>
-   </div>
-   <h1>Dropping in {{selectedSquare.name}}</h1>
- </div>
+  <div class="map">
+    <div>
+      <div class="grid__horizontal">
+        <div>A</div>
+        <div>B</div>
+        <div>C</div>
+        <div>D</div>
+        <div>E</div>
+        <div>F</div>
+        <div>G</div>
+        <div>H</div>
+      </div>
+      <img class="map__image" :src="mapImage" alt="">
+      <div class="grid">
+        <template v-for="coord in mapData.grid">
+          <div class="grid__square" :key="coord"
+            :class="{'grid__square--selected': coord === selectedSquare.coord}">
+          </div>
+        </template>
+      </div>
+    </div>
+    <div class="controls">
+      <div class="repick">
+        <button v-on:click="() => {selectSquare()}">Pick again</button>
+      </div>
+      <h1 class="location">Dropping in {{selectedSquare.name}} - ({{selectedSquare.coord}})</h1>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -17,15 +35,19 @@ export default {
   props: ['mapImage', 'mapData'],
   data() {
     return {
-      selectedSquare: this.selectSquare(),
+      selectedSquare: {},
     };
+  },
+  created() {
+    this.selectSquare();
   },
   methods: {
     selectSquare() {
+      console.log('called');
       const locations = this.mapData.locations;
       const index = Math.random() * locations.length;
       const location = locations[Math.floor(index)];
-      return location;
+      this.$data.selectedSquare = location;
     },
   },
 };
@@ -34,16 +56,27 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
   .map {
-    height: 800px;
-    width: 800px;
+    display: grid;
+    grid-template-columns: 800px 1fr;
   }
   .map__image {
-    height: inherit;
+    height: 800px;;
     position: absolute;
     z-index: -10;
   }
+  .grid__horizontal {
+    display: grid;
+    grid-template-columns: repeat(8, 100px);
+    grid-template-rows: auto;
+  }
+
+  .grid__horizontal div {
+    justify-self: center;
+  }
+
   .grid {
     width: 800px;
+    height: 800px;
     display: grid;
     grid-template-columns: repeat(8, 100px);
     grid-template-rows: repeat(8, 100px);
@@ -55,5 +88,16 @@ export default {
 
   .grid__square--selected {
     background: rgba(0, 256, 0, 0.5);
+  }
+
+  .controls {
+    display: grid;
+    grid-template-rows: 100px 1fr;
+    text-align: center;
+  }
+
+  .location {
+    justify-self: center;
+    text-align: center;
   }
 </style>

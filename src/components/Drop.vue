5a7f3221ca68ccacba7d588be2589e7ae2fd6ffb
Alex Robertson <lex1989@gmail.com>
2018-01-24 21:55:42 +0000

    Changed title position

--- src/components/Drop.vue
+++ src/components/Drop.vue
@@ -1,48 +1,50 @@
 <template>
-  <div class="drop section columns">
-    <div class="map__container--outer column is-two-thirds">
-      <div class="grid__horizontal">
-        <div>A</div>
-        <div>B</div>
-        <div>C</div>
-        <div>D</div>
-        <div>E</div>
-        <div>F</div>
-        <div>G</div>
-        <div>H</div>
-      </div>
-      <div class="grid__vertical">
-        <div>I</div>
-        <div>J</div>
-        <div>K</div>
-        <div>L</div>
-        <div>M</div>
-        <div>N</div>
-        <div>O</div>
-        <div>P</div>
-      </div>
-      <div class="map__container--inner">
-        <img class="map__image image" :src="mapImage" alt="">
-        <div class="grid is-overlay">
-          <template v-for="coord in mapData.grid">
-            <div class="grid__square" :key="coord"
-              :class="{'grid__square--selected': coord === selectedSquare.coord}">
-            </div>
-          </template>
+  <div class="section drop">
+    <h1 class="title is-1 page__heading">
+      <router-link to="/">DROPZONE</router-link>
+    </h1>
+    <div class="columns">
+      <div class="map__container--outer column is-two-thirds">
+        <div class="grid__horizontal">
+          <div>A</div>
+          <div>B</div>
+          <div>C</div>
+          <div>D</div>
+          <div>E</div>
+          <div>F</div>
+          <div>G</div>
+          <div>H</div>
+        </div>
+        <div class="grid__vertical">
+          <div>I</div>
+          <div>J</div>
+          <div>K</div>
+          <div>L</div>
+          <div>M</div>
+          <div>N</div>
+          <div>O</div>
+          <div>P</div>
+        </div>
+        <div class="map__container--inner">
+          <img class="map__image image" :src="mapImage" alt="">
+          <div class="grid is-overlay">
+            <template v-for="coord in mapData.grid">
+              <div class="grid__square" :key="coord"
+                :class="{'grid__square--selected': coord === selectedSquare.coord}">
+              </div>
+            </template>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="controls column auto">
-      <h1 class="title is-1">
-        <router-link class="controls__link--home" to="/">DROPZONE</router-link>
-      </h1>
-      <div class="repick">
-        <button class="button is-primary" v-on:click="selectSquare">Pick again</button>
-      </div>
-      <div class="location">
-        <h2 class="title is-4">You should drop in:</h2>
-        <h2 class="title is-2">{{selectedSquare.name}}</h2>
-        <h2 class="title is-2">{{selectedSquare.coord}}</h2>
+      <div class="controls column auto">
+        <div class="repick">
+          <button class="button is-primary" v-on:click="selectSquare">Pick again</button>
+        </div>
+        <div class="location">
+          <h2 class="title is-4">You should drop in:</h2>
+          <h2 class="title is-2">{{selectedSquare.name}}</h2>
+          <h2 class="title is-2">{{selectedSquare.coord}}</h2>
+        </div>
       </div>
     </div>
   </div>
@@ -74,6 +76,18 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
   @import "~bulma/bulma.sass";
+  .page {
+    &__heading {
+      text-align: center;
+      > a {
+        color: black;
+        text-decoration: none;
+        &:hover {
+          color: $primary;
+        }
+      }
+    }
+  }
 
   .map {
     &__container {
@@ -81,12 +95,12 @@ export default {
         display: grid;
         grid-template-columns: 20px auto;
         grid-template-rows: 20px auto;
-        max-width: 90vh;
+        max-width: 80vh;
       }
       &--inner {
         position: relative;
         padding: 0;
-        max-width: 90vh;
+        max-width: 80vh;
 
       }
     }
@@ -134,25 +148,18 @@ export default {
 
   .controls {
     display: grid;
-    grid-template-rows: 100px 100px auto;
+    grid-template-rows: 50px auto;
     width: 100%;
     text-align: center;
-    &__link {
-      &--home {
-        text-decoration: none;
-        color: black;
-        &:hover {
-          color: $primary;
-        }
-      }
-    }
+    // align-self: center;
   }
 
   .repick {
-    margin-bottom: 10px;
+    padding-top: 18px;
   }
 
   .location {
+    padding-top: 18px;
     max-width: 100%;
   }
 </style>

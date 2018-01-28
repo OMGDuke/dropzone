14af0148031c57192bbed28c4bea3fbceeb0c2cd
OMGDuke <lex1989@gmail.com>
2018-01-28 21:00:40 +0000

    mobile nav now closes on click

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="navbar is-primary">
     <div class="navbar-brand">
-      <router-link class="navbar-item" to="/">
+      <router-link class="navbar-item" to="/" v-on:click.native="showNav = !showNav">
         <img class="map__image image" :src="logoWhite" alt="">&nbsp;
         <span>DROPZONE</span>
       </router-link>
@@ -16,7 +16,8 @@
     <div  class="navbar-menu" v-bind:class="{ 'is-active' : showNav }">
       <div class="navbar-start">
         <router-link to="/" class="navbar-item"
-          v-bind:class="{ 'is-active' : $route.path === '/' }">
+          v-bind:class="{ 'is-active' : $route.path === '/' }"
+          v-on:click.native="showNav = !showNav">
           Home
         </router-link>
         <div class="navbar-item has-dropdown is-hoverable">
@@ -25,16 +26,19 @@
           </span>
           <div class="navbar-dropdown is-boxed">
             <router-link class="navbar-item" to="/erangel"
-              v-bind:class="{ 'is-active' : $route.path === '/erangel' }">
+              v-bind:class="{ 'is-active' : $route.path === '/erangel' }"
+              v-on:click.native="showNav = !showNav">
               Erangel
             </router-link>
             <router-link class="navbar-item" to="/miramar"
-              v-bind:class="{ 'is-active' : $route.path === '/miramar' }">
+              v-bind:class="{ 'is-active' : $route.path === '/miramar' }"
+              v-on:click.native="showNav = !showNav">
               Miramar
             </router-link>
             <hr class="navbar-divider">
             <router-link class="navbar-item" to="/fortnite"
-              v-bind:class="{ 'is-active' : $route.path === '/fortnite' }">
+              v-bind:class="{ 'is-active' : $route.path === '/fortnite' }"
+              v-on:click.native="showNav = false">
               Fortnite
             </router-link>
           </div>
@@ -43,7 +47,8 @@
 
       <div class="navbar-end">
         <router-link to="/about" class="navbar-item"
-          v-bind:class="{ 'is-active' : $route.path === '/about' }">
+          v-bind:class="{ 'is-active' : $route.path === '/about' }"
+          v-on:click.native="showNav = false">
           About
         </router-link>
       </div>
